8c7d6307c603643d80080f385b497ad3b1c144b2
CVS2003 <clara.vej@outlook.com>
2024-04-10 13:01:27 +0200

    html filer til forside efter login samt til login plus get og post mapping til login

--- src/main/java/com/example/fullstackprojekt/Controller/WishController.java
+++ src/main/java/com/example/fullstackprojekt/Controller/WishController.java
@@ -1,13 +1,37 @@
 package com.example.fullstackprojekt.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class WishController {
 
     @GetMapping("/")
-    public String forside(){
+    public String index() {
+
         return "forside";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username,RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("username", username);
+
+        return "redirect:/loggingIn";
+
+    }
+
+    @GetMapping("/loggingIn")
+    public String loggedIn(@RequestParam("username") String username, Model model) {
+
+        model.addAttribute("username", username);
+
+        return "loggedIn";
+
+    }
+
 }
