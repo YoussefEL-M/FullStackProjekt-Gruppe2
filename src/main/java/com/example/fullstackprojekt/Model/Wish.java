4e1cfb8cb415ab45d7de589f2131bf64ffcb24f4
CVS2003 <clara.vej@outlook.com>
2024-04-10 10:56:25 +0200

    Model for User og Wish plus ny maven dependency

--- src/main/java/com/example/fullstackprojekt/Model/Wish.java
+++ src/main/java/com/example/fullstackprojekt/Model/Wish.java
@@ -1,4 +1,79 @@
 package com.example.fullstackprojekt.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Wish {
+
+    @Id
+    private int id;
+    private String name;
+    private double price;
+    private int amount;
+    private String description;
+    private String url;
+
+    public Wish() {
+
+    }
+
+    public Wish(int id, String name, double price, int amount, String description, String url) {
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.description = description;
+        this.url = url;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
