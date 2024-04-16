9252a4eaf898effddda9b745d88ef8126f9009cf
CVS2003 <clara.vej@outlook.com>
2024-04-16 13:21:09 +0200

    Design af forside

--- src/main/java/com/example/fullstackprojekt/Controller/WishController.java
+++ src/main/java/com/example/fullstackprojekt/Controller/WishController.java
@@ -48,6 +48,8 @@ public class WishController {
         model.addAttribute("username", username);
         model.addAttribute("password", password);
 
+
+
         return "redirect:/wishlist";
 
     }
